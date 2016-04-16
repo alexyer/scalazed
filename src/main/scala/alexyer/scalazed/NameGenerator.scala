8a87c81afc4fcc49870e91c6bc7a0f202c192660
Olexander Yermakov <mannavard1611@gmail.com>
2016-04-16 12:28:18 +0300

    Remove hardcoded path

--- src/main/scala/alexyer/scalazed/NameGenerator.scala
+++ src/main/scala/alexyer/scalazed/NameGenerator.scala
@@ -6,12 +6,12 @@ import scala.util.control.Breaks._
 /**
   * Created by olexander.yermakov on 13.04.16.
   */
-class NameGenerator(chainLen: Int = 2, maxLen: Int = 9) {
+class NameGenerator(namesFile: String, chainLen: Int = 2, maxLen: Int = 9) {
   require(chainLen >= 2 && chainLen <= 9, "Chain length must be >=2 and <= 9")
 
   private val markovDict = new MarkovDict
 
-  Source.fromFile("names/male_and_neutral.txt").getLines.flatMap(_.split(" ")).foreach { name =>
+  Source.fromFile(namesFile).getLines.flatMap(_.split(" ")).foreach { name =>
     val s = " " * chainLen + name
     for (i <- 0 until name.length) {
       markovDict.addKey(s.slice(i, i+chainLen), s(i+chainLen).toString)
